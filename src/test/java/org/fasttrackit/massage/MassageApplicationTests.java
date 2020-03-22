9ada2102b7c3ca2b227e667abdf9aa879f9668dd
Alex <andreica.alexandru2000@gmail.com>
2020-03-22 17:37:40 +0200

    first commit

---
+++ src/test/java/org/fasttrackit/massage/MassageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.massage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
